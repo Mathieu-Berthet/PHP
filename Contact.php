1a4960db57c341c648aa59dc99bba793286f7aa4
Mathieu <berthetmathieu@free.fr>
2023-01-03 16:28:28 +0100

    Début de la récupération des données

--- Contact.php
+++ Contact.php
@@ -1,23 +1,35 @@
 <?php
+    $civilite = "";
+    $name = "";
+    $firstName = "";
+    $email = "";
+    $raisonContact = "";
+    $message = "";
+
+
+    if(filter_input($_POST['nom']) == "")
+    {
+        echo "test";
+    }
 ?>
 
-<form action="" method="GET">
+<form action="" method="post">
     <p>Veuillez saisir vos informations ainsi que votre message afin de nous contacter : </p>
-    <label>Civilité : </label>
+    <label>Votre Civilité : </label>
     <select>
         <option value="Monsieur">Monsieur</option>
         <option value="Madame">Madame</option>
-    </select><br>
+    </select><br><br>
 
-    <label>Votre nom : </label><input type="text" name="nom" placeholder="nom"><br><br>
-    <label>Votre Prénom : </label><input type="text" name="prenom" placeholder="prénom"><br><br>
-    <label>Votre email : </label><input type="email" name="mail" placeholder="email"><br><br>
+    <label>Votre Nom : </label>&nbsp;<input type="text" name="nom" placeholder="Nom"><br><br>
+    <label>Votre Prénom : </label>&nbsp;<input type="text" name="prenom" placeholder="Prénom"><br><br>
+    <label>Votre E-mail : </label>&nbsp;<input type="email" name="mail" placeholder="Email@email.com"><br><br>
     <label>Veuillez indiquer la raison pour laquelle vous nous contacter : </label><br><br>
-    <input type="radio" name="raisonContact"><label>Proposition d'emploi</label><br>
-    <input type="radio" name="raisonContact"><label>Demande d'information</label><br>
-    <input type="radio" name="raisonContact"><label>Prestation</label><br><br>
+    <input type="radio" name="raisonContact">&nbsp;<label>Proposition d'emploi</label><br>
+    <input type="radio" name="raisonContact">&nbsp;<label>Demande d'information</label><br>
+    <input type="radio" name="raisonContact">&nbsp;<label>Prestation</label><br><br>
 
-    <label>Votre message : </label><br><textarea placeholder="Votre message"></textarea><br><br>
+    <label>Votre message : </label><br><textarea placeholder="Votre message" style="resize:none"></textarea><br><br>
 
     <input type="submit" value="Envoyer">
 
